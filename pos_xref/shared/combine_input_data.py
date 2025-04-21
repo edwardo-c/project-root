248ac0db45df1a27ad7f657a553bb51e629cb5c0
EdwardoCallejas <e.callejas14@yahoo.com>
2025-04-21 11:31:34 -0500

    Refactor: Consolidate dataframe standardization logic
    Moved repeated logic into a single function to reduce redundancy.
    Added conditional handling for customer types.

--- pos_xref/shared/combine_input_data.py
+++ pos_xref/shared/combine_input_data.py
@@ -1,8 +1,55 @@
 import pandas as pd
-from pos_xref.shared.pos_config import get_raw_pos_customers
-from pos_xref.shared.direct_config import capture_direct_customers_df
+import pos_xref.shared.direct_config as dc
+import pos_xref.shared.pos_config as pc
+import pos_xref.shared.utils as u
+import utils.utils_io as uio
+
+STANDARDIZED_COLUMNS = ['acct_num', 'customer_name', 'acct_group', 'bill_to_zip', 'bill_to_state']
 
 def return_input_data(test_env:bool):
-    direct_customers_df = capture_direct_customers_df(test_env=test_env)
-    pos_xref_df = get_raw_pos_customers(year=2025, test_env=test_env)
-    return pd.concat([direct_customers_df, pos_xref_df])
+    
+    files_info = [dc.DirectCustomersFileInfo(test_env), pc.POSCustomersFileInfo(2024,test_env)]
+    output_df = pd.DataFrame()
+
+    for info in files_info:
+        df = _standardize_dataframe(info)
+        output_df = pd.concat([df, output_df])
+    
+    return output_df
+
+def _standardize_dataframe(info) -> pd.DataFrame:
+
+    # create a safe (local copy) version, return the file path and data frame
+    safe_path_info = uio.read_safe_excel_file(
+        info.file_path(), 
+        sheet_name=info.ws, 
+        header_row=info.header_row()
+    )
+
+    # unpackage safe copy data frame
+    df = safe_path_info['safe_data_frame']
+
+    # confirm expected headers existence
+    u._confirm_headers(df.columns, info.expected_columns())
+
+    # truncate data frame to pertinent columns only
+    df = df[info.expected_columns()]
+
+    # customer type specific data frame modifications; 
+    # for now only the renaming of columns is different
+    if info.cust_type == "Direct":
+        df = df.rename(columns=u._column_rename_map(df.columns, STANDARDIZED_COLUMNS))
+    elif info.cust_type == "POS":
+        df = pc.rename_columns(df)
+    else:
+        raise KeyError(f"unspecified customer type")
+
+    # convert remaining columns to string type data types
+    df = df.astype({st_col:str for st_col in df.columns})
+
+    df = df.drop_duplicates()
+
+    # delete safe version
+    uio.del_safe_path(safe_path_info)
+
+    return df
--- pos_xref/shared/utils.py
+++ pos_xref/shared/utils.py
@@ -2,9 +2,8 @@
 shared utility functions for consolidating input data
 '''
 from typing import Dict
-import pandas as pd
 
-def _build_column_rename_map(curr_cols, st_cols) -> Dict:
+def _column_rename_map(curr_cols, st_cols) -> Dict:
     '''build a dict to rename current columns to standardized columns (st_cols)
     '''
     return {col: st_col for col, st_col in zip(curr_cols, st_cols)}
@@ -14,11 +13,3 @@ def _confirm_headers(cols, expected_columns) ->bool:
         if not header in cols:
             raise KeyError(f"Expected header {header} not found")
     return True
-
-def _convert_columns_dtypes(df: pd.DataFrame, cols_to_update: Dict) -> pd.DataFrame:
-    '''
-    Loop through data frame converting columns to specified data type
-    '''
-    df = df.astype(cols_to_update)
-
-    return df
