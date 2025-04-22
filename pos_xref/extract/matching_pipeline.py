0c17fffebbec4ccdcea950f1e284ae1d807d83cd
EdwardoCallejas <e.callejas14@yahoo.com>
2025-04-22 14:53:04 -0500

    Checkpoint: refactor of safe-file-path
    utils_io func safe_path now accepts csv and xlsx file types

--- pos_xref/extract/matching_pipeline.py
+++ pos_xref/extract/matching_pipeline.py
@@ -4,16 +4,12 @@ import utils.utils_io as uio
 from pos_xref.shared.processed_matches_config import ProcessedMatches
 
 
-def get_previous_matches(test_env: bool) :
+def get_processed_matches(test_env: bool) :
 
     info = ProcessedMatches(test_env=test_env)
 
     # create a safe (local copy) version, return the file path and data frame
-    safe_path_info = uio.read_safe_csv_file(
-        info.file_path(), 
-        sheet_name=info.ws, 
-        header_row=info.header_row()
-    )
+    safe_path_info = uio.read_safe_excel_file(orig_file_path=info.file_path(), ftype='csv')
 
     # unpackage safe copy data frame
     df = safe_path_info['safe_data_frame']
--- utils/utils_io.py
+++ utils/utils_io.py
@@ -4,7 +4,7 @@ import tempfile
 import os
 from typing import Dict
 
-def read_safe_excel_file(orig_file_path: str, sheet_name: str, header_row: int = 0) -> Dict:
+def read_safe_excel_file(orig_file_path: str, sheet_name: str = '', header_row: int = 0, ftype='xlsx') -> Dict:
     '''
     Makes a temporary copy of the Excel file and reads from that instead.
     
@@ -24,34 +24,14 @@ def read_safe_excel_file(orig_file_path: str, sheet_name: str, header_row: int =
         shutil.copy2(orig_file_path, temp_path)
 
         # read temporary file data frame
-        df = pd.read_excel(temp_path, sheet_name=sheet_name, header=header_row)
-
-        return {'temp_file_path': temp_path, 'safe_data_frame': df}
-    
-    except Exception as e:
-        raise RuntimeError(f"Failed to read Excel file safely: {e}")
-
-def read_safe_csv_file(orig_file_path: str, sheet_name: str, header_row: int = 0) -> Dict:
-    '''
-    Makes a temporary copy of the Excel file and reads from that instead.
-    
-    Args:
-        file_path (str): Original Excel file path.
-        sheet_name (str): name of the worksheet to read
-        header_row (int): row index for headers
-    Returns:
-        Dict containing file path to temp file and dataframe captured from file
-    '''
-    try:
-        # create a temporary file path
-        temp_dir = tempfile.gettempdir()
-        temp_path = os.path.join(temp_dir, os.path.basename(orig_file_path))
-
-        # copy original file to temp
-        shutil.copy2(orig_file_path, temp_path)
-
-        # read temporary file data frame
-        df = pd.read_csv(temp_path)
+        if ftype == 'xlsx':
+            if not (sheet_name) and (not header_row):
+                raise KeyError(f"missing sheet name or header row for {ftype} file")
+            df = pd.read_excel(temp_path, sheet_name=sheet_name, header=header_row)
+        elif ftype == 'csv':
+            df = pd.read_csv(temp_path)
+        else:
+            raise KeyError(f"{ftype} is an invalid file type")
 
         return {'temp_file_path': temp_path, 'safe_data_frame': df}
     
