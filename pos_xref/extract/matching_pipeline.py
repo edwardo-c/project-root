539d86ecd3ac4e0873c05977b2bd145bbba6d0b3
EdwardoCallejas <e.callejas14@yahoo.com>
2025-04-23 18:56:05 -0500

    Completed refactor: previous matches
    captures previous matches, unreviewed matches, and mismatches; converts to dict[str:list] structure for O(1) lookups

--- pos_xref/extract/matching_pipeline.py
+++ pos_xref/extract/matching_pipeline.py
@@ -1,39 +1,61 @@
 import pandas as pd
-import pos_xref.shared.processed_matches_config 
+from collections import defaultdict
+from typing import Dict
+from rapidfuzz import process
 import utils.utils_io as uio
 from pos_xref.shared.processed_matches_config import ProcessedMatches
+from pos_xref.shared.matches_config import Matches
 
 
-def get_processed_matches(test_env: bool) :
-
-    info = ProcessedMatches(test_env=test_env)
-
-    # create a safe (local copy) version, return the file path and data frame
-    safe_path_info = uio.read_safe_excel_file(orig_file_path=info.file_path(), ftype='csv')
-
-    # unpackage safe copy data frame
-    df = safe_path_info['safe_data_frame']
-
-    lookup_structure = _lookup_structure(df)
-    print(lookup_structure)
-
-    # delete safe version
-    uio.del_safe_path(safe_path_info)
-
-    return lookup_structure
-
-def _lookup_structure(df):
-    # create dictionary of normalized name, sold_to_name
-    d = {}
-    for n_name, o_name  in zip(df['normalized_name'], df['customer_name']):
-        if not n_name in d.keys():
-            d[n_name] = o_name
-    return d
+def get_previous_matches(test_env: bool) -> Dict[str, list]:
+    '''
+    returns a dict(list) to test if a fuzzy match has been matched before
+    processed matches: confirmed matches, mismatch, or unreviewed
+    example:
+        {
+            amazon: [AMAZON, amazon inc, Amazon.com] (matches)
+            apple: [Appleson, Applewood] (mismatches)
+            netflix: [netflix branch ca, netfloxan] (match and mismatch)
+        }
+    the type of match is irrelavent, only checking if previously matched
+    '''
+    # concat data frames of matches and processed
+    previously_reviewed_info = [ProcessedMatches(test_env=test_env), Matches(test_env=test_env)]
+    previously_matched_df = pd.DataFrame()
 
 
+    # get pd, concat to final df, delete safe file
+    for info in previously_reviewed_info:
+        # create a safe (local copy) version, return the file path and data frame
+        safe_path_info = uio.read_safe_excel_file(orig_file_path=info.file_path(), ftype='csv')
+        df = safe_path_info['safe_data_frame'] # extract data
+        uio.del_safe_path(safe_path_info) # delete safe version
+        previously_matched_df = pd.concat([df, previously_matched_df]) # concat to final df
 
-# find new matches that have not been previously matched
-    
+    # convert data frame into look up structure
+    processed_matches = defaultdict(list)
+    for norm, original in zip(previously_matched_df['normalized_name'], previously_matched_df['customer_name']):
+        processed_matches[norm].append(original)
+
+    return processed_matches
 
 
-# refresh processed_matches data frame with new matches
+def new_matches(norm_name, all_norm_names, name_mapping: Dict, previous_matches):
+    '''
+    Return fuzzy matches for a given customer, excluding matches previously completed
+    args:
+        all_customers: the customers to query against
+
+    Query against normalized names; return customer_name
+    exclude self matches
+    exclude previously matched
+    '''
+    raw_matches = process.extract(query=norm_name, choices=all_norm_names, score_cutoff=88, limit=20)
+    new_matches = []
+    # remove previous matches
+    for match in raw_matches:
+        if (norm_name, match[0]) not in previous_matches:
+            '''append original, non-normalized name to return matches'''
+            new_matches.append(name_mapping[match[0]])
+
+    return new_matches
