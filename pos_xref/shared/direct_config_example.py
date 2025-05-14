356b2ad9725aebae3cbc0f3bbe9cb06f6db2b0f0
EdwardoCallejas <e.callejas14@yahoo.com>
2025-05-14 11:43:25 -0500

    added normalized_name vectorized  logic to direct df in FileReader Class

--- pos_xref/shared/direct_config_example.py
+++ pos_xref/shared/direct_config_example.py
@@ -1,6 +1,8 @@
 import os
-from utils import utils_io as uio
 import pandas as pd
+from pos_xref.shared.utils import column_rename_map
+import pos_xref.transformations.normalize_data as nd
+from utils import utils_io as uio
 
 
 def test_file_details(ftype: str) -> dict:
@@ -20,10 +22,12 @@ def test_file_details(ftype: str) -> dict:
     d = {
         'direct' : {'file_path': r"test\path\pos_xref\data\raw\direct_customers_export.xlsx",
                     'sheet_name': 'Data',
-                    'header_row': 0},
+                    'header_row': 0,
+                    'columns': ['Customer ID', 'Customer Name', 'State', 'Postal Code', 'Account Group']},
         'pos_sales': {'file_path': r"C:\test\path\pos_xref\data\raw\Incentive Comp - 2025.xlsx", 
                     'sheet_name': 'POS',
-                    'header_row': 5}, 
+                    'header_row': 5,
+                    'columns':['SoldToName', 'BillToCustomerZip', 'BillToCustomerState']}, 
         'matches' : {'file_path': r"C:\test\path\pos_xref\data\output\matches.csv"},
         'processed' : {'file_path': r"C:\test\path\pos_xref\data\processed\processed_matches.csv"}
     }
@@ -67,6 +71,7 @@ class FileReader():
         self.file_path = self.file_details.get('file_path')
         self.sheet_name = self.file_details.get('sheet_name', None)
         self.header_row = self.file_details.get('header_row', 0)
+        self.columns = self.file_details.get('columns', 0)
         self.file_ext = os.path.splitext(os.path.basename(self.file_path))[1]
     
     def read_df(self) -> pd.DataFrame:
@@ -74,14 +79,40 @@ class FileReader():
                                         sheet_name=self.sheet_name,
                                         header_row=self.header_row,
                                         ftype=self.file_ext)
-        self.df = info['safe_data_frame']
+        
+        self.df = info['safe_data_frame'] # unpackage data frame from info
+        # truncate to pertinent columns only
         uio.del_safe_path(info)
+
+    def _normalize_df(self):
+        '''
+        direct and pos_sales requiring standardization of column names
+        '''
+        # TODO: Identify other normalization steps required for each data frame  
+        if self.ftype == "direct":
+            
+            # rename columns and truncate to only the renamed columns
+            STANDARDIZED_COLUMNS = ['acct_num', 'customer_name', 'bill_to_state', 'bill_to_postal_code', 'acct_group']
+            self.df = self.df.rename(columns=column_rename_map(self.columns, STANDARDIZED_COLUMNS))[STANDARDIZED_COLUMNS]
+            # add normalized name series
+            self.df['normalized_name'] == self.df['customer_name'].apply(nd.normalize_name)
+
+        elif self.ftype == "pos_sales":
+            ...
+
+        # add normalized name
+        
+
+        return ""
+        
+    # def _normalize_data():
+    #     ...
 
     def run(self):
         self.file_path_details()
         self.load_file_info()
         self.read_df()
-
+        self._normalize_df()
 
         
 
