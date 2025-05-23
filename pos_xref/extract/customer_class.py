32fdd512457fcda68a7296163812dae8c02a96b8
EdwardoCallejas <e.callejas14@yahoo.com>
2025-05-23 15:56:40 -0500

    Feature: process data for output.
    fuzzy match and self match data frames are prepared and can be concatenated with existing files

--- pos_xref/extract/customer_class.py
+++ pos_xref/extract/customer_class.py
@@ -5,34 +5,31 @@ import pandas as pd
 from pos_xref.extract.previous_matches import PreviousMatches
 from pos_xref.extract.file_reader import FileReader
 
-# may need a customer class? would make it easier to fill the output df
-# use self.acct_num instead of trying to to paste it from a previous row.
-
-
-
-'''
-In order to update processed.csv - 
-
-fuzzy match direct to pos
-    Exclude previous matches
-    convert to output df
-
-fuzzy match pos to pos - concat with output df
-    Exclude self_matches
-    convert to output df
-
-'''
-
 class Customer():
-    ...
+    '''
+    what is needed in the output?
+    '''
+    def __init__(self, acct_num, norm, orig, bill_to_state, bill_to_postal_code, fuzzy_matches):
+        self.acct_num = acct_num
+        self.norm = norm
+        self.orig = orig
+        self.bill_to_state = bill_to_state
+        self.bill_to_postal_code = bill_to_postal_code
+        self.fuzzy_matches = fuzzy_matches
+
+    def __str__(self):
+        return f" Account Number: {self.acct_num} ; Customer Name: {self.orig}"
 
 class ProcessedOutput():
     def __init__(self, test_env):
         self.test_env = test_env
-        self.output_df = pd.DataFrame()
+        self.fuzzy_matches_df = pd.DataFrame()
+        self.self_matches_df = pd.DataFrame()
         self._prev_matches = {}
+        # self.reverse_name_map = {}
         self._direct_custs = pd.DataFrame()
         self._pos_custs = pd.DataFrame()
+        self.name_map = {}
 
     def __str__(self):
         return f"Output data for Processed.csv"
@@ -54,7 +51,7 @@ class ProcessedOutput():
         pos_sales.run()
         self._pos_custs = pos_sales.df    
 
-    def _fuzzy_match(self, norm, choices, name_map:Dict):
+    def _fuzzy_match(self, norm, choices):
         '''
         Return fuzzy matches for a given customer, excluding matches previously completed
         args:
@@ -70,54 +67,84 @@ class ProcessedOutput():
         
         # remove previous matches
         for match in raw_matches:
-            orig = name_map[match[0]]
+            orig = self.name_map[match[0]]
             if not orig in self._prev_matches[norm]: 
                 '''append original, non-normalized name to return matches'''
-                new_matches.append(orig)
+                new_matches.append(match)
 
         return new_matches
 
-    def _build_name_map(self) -> Dict:
+    def _build_name_maps(self) -> Dict:
         '''
         builds a name map from normalized_name to customer_name
         '''
-        return {norm : orig for norm, orig in zip(self._pos_custs['normalized_name'], self._pos_custs['customer_name'])}
+        self.name_map = {norm : orig for norm, orig in zip(self._pos_custs['normalized_name'], self._pos_custs['customer_name'])}
+        # self.reverse_name_map = {orig : norm for orig, norm in zip(self._pos_custs['customer_name'], self._pos_custs['normalized_name'])}
 
     def _matching_engine(self):
         '''
         Generate fuzzy match column
         '''
          # fuzzy matching for direct to pos
-        name_map = self._build_name_map() # used for mapping back to original name
         self._direct_custs['fuzzy_matches'] = self._direct_custs['normalized_name'].apply(
                                                                                         lambda norm: 
                                                                                         self._fuzzy_match(
                                                                                             norm, 
                                                                                             self._pos_custs['normalized_name'],
-                                                                                            name_map)
+                                                                                            )
                                                                                         )
+        
         # fuzzy matching for pos to pos
         self._pos_custs['fuzzy_matches'] = self._pos_custs['normalized_name'].apply(
                                                                                     lambda norm: 
                                                                                     self._fuzzy_match(
                                                                                         norm, 
                                                                                         self._pos_custs['normalized_name'],
-                                                                                        name_map)
                                                                                         )
+                                                                                    )
+
+    def _build_output_dfs(self):
+        '''
+            Convert fuzzy matches into output data frame; building cross reference (one to one)
+        '''
+        temp_df = pd.concat([self._pos_custs, self._direct_custs])
+
+        # fill customer class
+        customers = []
+        for index, row in temp_df.iterrows():                
+            customers.append(Customer(row['acct_num'], row['normalized_name'], row['customer_name'], 
+                                      row['bill_to_state'], row['bill_to_postal_code'], row['fuzzy_matches']))
         
-        # TODO: do you need fuzzy matching for pos back to direct? this could close the matching loop
-        # and would help the user match pos back to direct since all connections would be completed
+        # create output df
+        for cust in customers:
+            for fuzzy_match in cust.fuzzy_matches:
+                match_score = fuzzy_match[1]
+                if match_score < 100:
+                    row_index = len(self.fuzzy_matches_df)
 
+                    self.fuzzy_matches_df.loc[row_index, 'acct_num'] = cust.acct_num
+                    self.fuzzy_matches_df.loc[row_index, 'normalized_name'] = cust.norm
+                    self.fuzzy_matches_df.loc[row_index, 'customer_name'] = cust.orig
+                    self.fuzzy_matches_df.loc[row_index, 'bill_to_state'] = cust.bill_to_state
+                    self.fuzzy_matches_df.loc[row_index, 'bill_to_postal_code'] = cust.bill_to_postal_code
+                    self.fuzzy_matches_df.loc[row_index, 'fuzzy_match'] = self.name_map[fuzzy_match[0]]
+                    self.fuzzy_matches_df.loc[row_index, 'match_score'] = round(fuzzy_match[1],2)
+                else:
+                    r_i = len(self.self_matches_df)
 
-    def _build_output(self):
-        '''match direct to pos'''
-        
-        '''match pos to pos'''
+                    self.self_matches_df.loc[r_i, 'acct_num'] = cust.acct_num
+                    self.self_matches_df.loc[r_i, 'normalized_name'] = cust.norm
+                    self.self_matches_df.loc[r_i, 'customer_name'] = cust.orig
+                    self.self_matches_df.loc[r_i, 'match_type'] = 'self_match'
 
-        '''store output df as self.output_df'''
-        ...
+    def format_output_dfs(self):
+        self.self_matches_df.sort_values(by=['normalized_name'], inplace=True)
+        self.fuzzy_matches_df.sort_values(by=['normalized_name'], inplace=True)
 
     def run(self):
         self._requirements()
+        self._build_name_maps()
         self._matching_engine()
+        self._build_output_dfs()
+        self.format_output_dfs()
     
